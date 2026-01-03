10b5de3b8ca2450f3647e243302f208f952d9381
javsan77 <javsan77@gmail.com>
2026-01-03 05:03:08 +0000

    Deploying to gh-pages from @ javsan77/javsan77.github.io@650a0c9186bdb22bfe96b9a2b9b735fa0ad1b0e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,6 +417,11 @@ ninja.data = [{
           description: "FastAPI Base API (SQL Server + JWT Auth)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
+            },},{id: "projects-fastapi-base-api",
+          title: 'FastAPI Base API',
+          description: "FastAPI Base API (SQL Server + JWT Auth)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
